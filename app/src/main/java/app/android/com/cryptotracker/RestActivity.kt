7a8d1f30b32f37cf5ed67182c69df259041f0b08
birandkoray <birandkoray@gmail.com>
2018-09-18 20:33:18 +0300

    progress bar shown while rest api is being consumed and hidden when it's finished

--- app/src/main/java/app/android/com/cryptotracker/RestActivity.kt
+++ app/src/main/java/app/android/com/cryptotracker/RestActivity.kt
@@ -5,16 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
+import android.view.View
+import android.widget.ProgressBar
 import app.android.com.cryptotracker.adapter.ListAdapter
 import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
-import kotlinx.android.synthetic.main.activity_rest.*
 import okhttp3.*
 import java.io.IOException
 
 class RestActivity : AppCompatActivity() {
 
     var itemList: List<Item> = ArrayList<Item>()
+
+    var recyclerView: RecyclerView? = null
+
+    var progressBar: ProgressBar? = null
 
     object Constant {
         val apiBaseURL = "https://api.coinmarketcap.com"
@@ -24,15 +29,14 @@ class RestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rest)
-        val recyclerView = findViewById<RecyclerView>(R.id.my_recycler_view)
-        run()
-        Thread.sleep(2000)
-        recyclerView.adapter = ListAdapter(this,itemList)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-
+        recyclerView = findViewById(R.id.my_recycler_view)
+        progressBar = findViewById(R.id.pBar)
+        progressBar?.visibility = View.VISIBLE
+        recyclerView?.layoutManager = LinearLayoutManager(this)
+        restCall()
     }
 
-    fun run() {
+    fun restCall() {
         val client = OkHttpClient()
         val request = Request.Builder()
                 .url(Constant.apiBaseURL + "/v1/ticker/?limit=10")
@@ -50,6 +54,10 @@ class RestActivity : AppCompatActivity() {
                         val body = response.body()?.string()
                         val gson = GsonBuilder().create()
                         itemList = gson.fromJson<List<Item>>(body , object : TypeToken<List<Item>>() {}.type)
+                        runOnUiThread {
+                            recyclerView?.adapter = ListAdapter(this@RestActivity , itemList)
+                            progressBar?.visibility = View.GONE
+                        }
                     }
 
                 })
