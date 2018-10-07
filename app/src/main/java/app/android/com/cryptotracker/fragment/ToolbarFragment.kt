24b750ef88538968b9f86a1866ba28b8e497ff7b
birandkoray <birandkoray@gmail.com>
2018-10-07 13:33:26 +0300

    toolbar put into a fragment

--- app/src/main/java/app/android/com/cryptotracker/fragment/ToolbarFragment.kt
+++ app/src/main/java/app/android/com/cryptotracker/fragment/ToolbarFragment.kt
@@ -31,12 +31,12 @@ class ToolbarFragment : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater?.inflate(R.layout.toolbar_layout ,
-                container , true) as View
+        val view = inflater?.inflate(R.layout.toolbar_layout,
+                container, true) as View
 
         fbAuth.addAuthStateListener {
             if (fbAuth.currentUser == null) {
-                activity.startActivity(Intent(context , MainActivity::class.java))
+                activity?.finish()
             }
         }
 
