697c41f52254497008cff88683b7eea9733009de
birandkoray <birandkoray@gmail.com>
2018-10-07 21:18:16 +0300

    confirm dialog customized

--- app/src/main/java/app/android/com/cryptotracker/constants/DialogUtil.kt
+++ app/src/main/java/app/android/com/cryptotracker/constants/DialogUtil.kt
@@ -4,21 +4,31 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.content.Context
 import android.content.DialogInterface
+import android.support.v4.content.ContextCompat
 import app.android.com.cryptotracker.R
 
 object DialogUtil {
 
     fun logoutConfirmDialog(context: Context,
-                                    positiveButtonId : Int,
-                                    positiveButtonAction: DialogInterface.OnClickListener,
-                                    negativeButtonId: Int,
-                                    negativeButtonAction: DialogInterface.OnClickListener?
-                                    ): Dialog {
-        val builder = AlertDialog.Builder(context)
+                            positiveButtonId: Int,
+                            positiveButtonAction: DialogInterface.OnClickListener,
+                            negativeButtonId: Int,
+                            negativeButtonAction: DialogInterface.OnClickListener?
+    ): Dialog {
+        val builder = AlertDialog.Builder(context, R.style.Theme_AppCompat_DayNight_Dialog_Alert)
         builder.setMessage(R.string.logoutconfirm)
-                .setPositiveButton(positiveButtonId,positiveButtonAction)
-                .setNegativeButton(negativeButtonId,negativeButtonAction)
+                .setPositiveButton(positiveButtonId, positiveButtonAction)
+                .setNegativeButton(negativeButtonId, negativeButtonAction)
         // Create the AlertDialog object and return it
-        return builder.create()
+        val alertDialog = builder.create()
+
+        alertDialog.setOnShowListener {
+            val dialog = it as AlertDialog
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(ContextCompat.getColor(context, R.color.colorPrimaryDark))
+            dialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(ContextCompat.getColor(context, R.color.green));
+
+        }
+
+        return alertDialog
     }
 }
