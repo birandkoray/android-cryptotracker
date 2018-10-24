6326ad02d0f1000a934a8e1c74c7b9814688d8da
birandkoray <birandkoray@gmail.com>
2018-10-24 18:41:07 +0300

    rest api of cryptotracker app changed

--- app/src/main/java/app/android/com/cryptotracker/constants/Constants.kt
+++ app/src/main/java/app/android/com/cryptotracker/constants/Constants.kt
@@ -1,7 +1,7 @@
 package app.android.com.cryptotracker.constants
 
 object Constants {
-    val APIURL = "https://api.coinmarketcap.com/v1/ticker/?limit=10"
+    val APIURL = "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?limit=10"
     val EMAIL = "XXXXXX"
     val PASSWORD = "YYYYY"
     val NETWORK_ERROR_MSG = "NO INTERNET CONNECTION"
